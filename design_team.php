9c65f878859032b3e695ef26f34417e3e24165cc
Ilambharathi Kanniah <ilambhrathik@gmail.com>
2013-11-23 17:28:35 -0500

    Credits for the HCL Infosystem Information Portal.

---
+++ design_team.php
@@ -0,0 +1,8 @@
+<?php
+include_once("header.php");
+echo "<h1>Design Team</h1>";
+echo "<h2>As part of our internship project , we developed this Info Portal having the features of Notices,Message Board.<br/><br/>The Design Team :</h2>";
+echo "<h3><ul><li>K.Ilambharathi</li></ul><ul><li>Sathish G.P</li></ul><ul><li>S.Suganth Krishna</li></ul></h3>";
+echo "<h2>We are students of <a href=http://www.iiitdm.ac.in><u>IIITD&M Kancheepuram</u></a>.</h2>";
+include_once("footer.php");
+?>
