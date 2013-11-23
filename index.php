35d4d7c314f4b4fe133d3159c1bbc384e9f2e342
Ilambharathi Kanniah <ilambhrathik@gmail.com>
2013-11-23 17:28:09 -0500

    Main index file.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include_once("header.php");
+echo "<br/><br/><h1>Welcome to the Info Portal of HCL Infosystems</h1><br/>";
+echo "<br/><h2>Introduction</h2><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This Portal is aimed at providing the employees with a platform to communicate with each other with relative ease. </p>
+<br/><br/><h2>Notices</h2><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It allows the employees to view notices, categorised by their content without having to move from their respective workstations. </p>
+<br/><br/><h2>Forum</h2><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;The HCL Forum enables employees to discuss any issue by posting messages. Employees can then reply to the post which considerably reduces the time required to resolve the issue.</p><br/><br/> ";
+include_once("footer.php");	
+?>
